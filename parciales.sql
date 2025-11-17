52de9b9dec2a8bd027ab3190f74d46f072173e96
Tomas <tbuhler@frba.utn.edu.ar>
2025-11-17 16:17:33 -0300

    mas parciales

--- parciales.sql
+++ parciales.sql
@@ -398,10 +398,310 @@ Realizar el o los objetos de base de datos necesarios para que dado un codigo de
 la mayor cantidad de dias consecutivos a partir de esa fecha que el producto tuvo al menos la venta de una unidad en el dia, 
 el sistema de ventas on line esta habilitado 24-7 por lo que se deben evaluar tidos los dias incluyendo domingos y feriados
 */
-create function diasSeguidos (@producto char(8), @fechaInicio smalldatetime)
-returns int
+
+
+/*
+Realizar un stored procedure que reciba un código de producto y una
+	fecha y devuelva la mayor cantidad de días consecutivos a partir de esa
+	fecha que el producto tuvo al menos la venta de una unidad en el día, el
+	sistema de ventas on line está habilitado 24-7 por lo que se deben evaluar
+	todos los días incluyendo domingos y feriados.
+*/
+
+create procedure diasSeguidos (@producto char(8), @fechaInicio smalldatetime, @dias int output)
 as 
 begin
-	
+	set @dias = 0
+	declare @seguidos int
+	set @seguidos = 0
+	declare @unaFecha smalldatetime, @fechaAnterior smalldatetime
+	declare c1 cursor for select fact_fecha from Item_Factura 
+							join Factura on item_tipo+item_sucursal+item_numero=fact_tipo+fact_sucursal+fact_numero
+							where item_producto = @producto and fact_fecha > @fechaInicio
+							group by item_producto, fact_fecha
+							order by fact_fecha
+	open c1
+	fetch c1 into @unaFecha -- 1, 2, 5, 6, 7
+	while @@FETCH_STATUS = 0
+	begin
+		if @fechaAnterior is null or datediff(day, @unaFecha, @fechaAnterior) = 1
+		begin
+			set @seguidos = @seguidos + 1
+			set @fechaAnterior = @unaFecha
+			if @dias < @seguidos
+				set @dias = @seguidos
+		end
+		else
+		begin
+			set @seguidos = 1
+		end
+		fetch c1 into @unaFecha
+	end
+	close c1
+	deallocate c1
+end
+go
+
+/*2. Actualmente el campo fact_vendedor representa al empleado que vendió
+la factura. Implementar el/los objetos necesarios para respetar
+integridad referenciales de dicho campo suponiendo que no existe una
+foreign key entre ambos.
+
+NOTA: No se puede usar una foreign key para el ejercicio, deberá buscar
+otro método */
+create trigger integridadVendedor on Factura for insert 
+as
+begin
+	if exists( select fact_vendedor from inserted
+		left join Empleado on fact_vendedor = empl_codigo
+		where empl_nombre is null
+	)
+	begin
+		print('No pueden insertarse facturas para vendedores no registrados')
+		rollback
+	end
+end
+go
+
+create trigger integridadVendedor on Empleado for delete 
+as
+begin
+	if exists( select fact_vendedor from deleted
+		join Factura on fact_vendedor = empl_codigo
+	)
+	begin
+		print('El empleado tiene facturas asignadas')
+		rollback
+	end
+end
+go
+
+/* 2. Cree el o los objetos necesarios para que controlar que un producto no pueda tener asignado 
+un rubro que tenga mas de 20 productos asignados, 
+si esto ocurre, 
+hay que asignarle el rubro que menos productos tenga asignado e informar a que producto y que rubro se le asigno.
+En la actualidad la regla se cumple y no se sabe la forma en que se accede a la Base de Datos.*/
+create trigger rubroCorrecto on Producto for insert, update
+as
+begin
+	declare @producto char(8), @detalle char(50), @precio decimal(12,2), @familia char(3), @rubro char(4), @envase numeric(6)
+	declare c1 cursor for select * from inserted
+	open c1
+	while @@FETCH_STATUS = 0
+	begin
+		if (select count(*) from Producto where prod_rubro = @rubro) + 1 > 20
+		begin
+			declare @nuevoRubro char(4)
+			set @nuevoRubro = (select top 1 rubr_id 
+							from Rubro 
+							left join Producto on prod_rubro = rubr_id
+							group by rubr_id order by count(*))
+			insert into Producto (prod_codigo, prod_detalle, prod_precio, prod_familia, prod_rubro, prod_envase)
+			values (@producto, @detalle, @precio, @familia, @nuevoRubro, @envase)
+			print('Se asigno el rubro '+ @nuevoRubro + ' al producto '+ @producto)
+		end
+		else
+		begin
+			insert into Producto (prod_codigo, prod_detalle, prod_precio, prod_familia, prod_rubro, prod_envase)
+			values (@producto, @detalle, @precio, @familia, @rubro, @envase)
+		end
+	end
+	close c1
+	deallocate c1
+end
+go
+
+select * from Producto
+go
+
+/*
+Implementar una regla de negocio en línea que al realizar una venta (SOLO INSERCION)
+permita componer los productos descompuestos,
+es decir, si se guardan en la factura 2 hamb, 2 papas 2 gaseosas se deberá guardar en la factura 2 (DOS) combo1, 
+Si 1 combo1 equivale a: 1 hamb. 1 papa y 1 gaseosa.
+
+Nota: Considerar que cada vez que se guardan los items, se mandan todos los productos de ese item a la vez, 
+y no de manera parcial.
+*/
+create trigger componerVenta on Item_Factura for insert
+as
+begin
+	declare @productoCompuesto char(8), @cantidadCombo int
+	declare c1 cursor for select comp_producto, max(item_cantidad / comp_cantidad) from inserted 
+						join Composicion co1 on item_producto = co1.comp_componente
+						group by comp_producto
+						having (select count(*) from Composicion co2 
+								where co1.comp_producto = co2.comp_producto) 
+							= count(*)
+	open c1
+	fetch c1 into @productoCompuesto, @cantidadCombo
+	while @@FETCH_STATUS = 0
+	begin
+		declare @componente char(8), @cantidadComponente int
+		declare c2 cursor for select comp_componente, comp_cantidad from inserted 
+						join Composicion on item_producto = comp_componente
+						where comp_producto = @productoCompuesto
+		open c2
+		fetch c2 into @componente, @cantidadComponente
+		while @@FETCH_STATUS = 0
+		begin
+			update Item_Factura set item_cantidad = item_cantidad - @cantidadComponente * @cantidadCombo
+			where @componente = item_producto
+			fetch c2 into @componente, @cantidadComponente
+		end
+		close c2
+		deallocate c2
+
+		insert into Item_Factura /* info combo */
+		fetch c1 into @productoCompuesto, @cantidadCombo
+	end
+	close c1
+	deallocate c1
+end
+go
+
+/* 2. Implementar los objetos necesarios para registrar, en tiempo real, los 10 productos
+mas vendidos por anio en una tabla especifica. Esta tabla debe contener exclusivamente la info requerida
+sin incluir filas adicionales. 
+
+Los mas vendidos se define como aquellos productos con el mayor numero de unidades vendidas.
+*/
+create table mas_vendidos (
+	prod_codigo char(8),
+	cantidad numeric(12,2),
+	anio int
+)
+go
+
+CREATE PROCEDURE registrar_mas_vendidos(@anio datetime2)
+AS
+BEGIN
+    INSERT INTO mas_vendidos(
+        prod_codigo,
+        cantidad,
+        anio
+    ) SELECT TOP 10 prod_codigo, SUM(item_cantidad), @anio
+    FROM Producto
+    JOIN Item_Factura ON item_producto = prod_codigo
+    JOIN Factura ON item_tipo+item_numero+item_sucursal = fact_tipo+fact_numero+fact_sucursal
+    WHERE year(fact_fecha) = @anio
+    GROUP BY prod_codigo
+    ORDER BY SUM(item_cantidad) desc
+END
+go
+/* 2. Se detectó un error en el proceso de registro de ventas, donde se almacenaron productos compuestos
+en lugar de sus componentes individuales. Para solucionar este problema, se debe:
+
+    1. Diseñar e implmenetar los objetos necesarios para reoganizar las ventas tal como están registradas actualmente 
+    2. Desagregar los productos compuestos vendidos en sus componenetes individuales, asegurando
+		que cada venta refleje correctamente los elementos que la compronen
+    3. Garantizar que la base de datos quede consistente y alineada con las 
+		especificaciones requeridas para el manejo de poductos
+*/
+create procedure corregirComposiciones
+as
+begin
+	declare @tipo char(1), @sucursal char(4), @numero char(8), @prodcompuesto char(8), @cantidad decimal(12,2)
+	declare c1 cursor for select 
+							item_tipo,
+							item_sucursal,
+							item_numero,
+							item_producto,
+							item_cantidad
+							from Item_Factura where item_producto in (select comp_producto from Composicion)
+	open c1
+	fetch c1 into @tipo, @sucursal, @numero, @prodcompuesto, @cantidad
+	while @@FETCH_STATUS = 0
+	begin
+		declare @componente char(8), @cantidadComponente decimal(12,2), @precioComponente decimal(12,2)
+		declare c2 cursor for select comp_componente, comp_cantidad from Composicion 
+							join Producto on comp_componente = prod_codigo
+							where comp_producto = @prodcompuesto
+								
+		open c2
+
+		while @@FETCH_STATUS = 0
+		begin
+			insert into Item_Factura (item_tipo, item_sucursal, item_numero, item_producto, item_cantidad, item_precio)
+			values (@tipo, @sucursal, @numero, @componente, @cantidad * @cantidadComponente, @precioComponente)
+		end
+		close c2
+		deallocate c2
+		delete from Item_Factura where item_tipo+item_sucursal+item_numero=@tipo+@sucursal+@numero and item_producto = @prodcompuesto
+	end
+	close c1
+	deallocate c1
+end
+go
+
+/*
+Implementar una regla de negocio en linea donde se valide que nunc?
+un producto compuesto pueda estar compuesto por componentes de rubros distintos a el.
+*/
+
+create trigger composicionDistintoRubro on Composicion for insert, update
+as
+begin
+	if exists(
+		select * from inserted 
+		join Producto p1 on comp_producto = p1.prod_codigo
+		join Producto p2 on comp_componente = p2.prod_codigo
+		where p1.prod_rubro <> p2.prod_rubro
+	)
+	rollback
+end
+go
+
+/*
+Se pide crear el/los objetos necesarios para que se imprima un cupon
+con la leyenda "Recuerde solicitar su regalo sorpresa en su próxima compra" a
+los clientes que, entre los productos comprados, hayan adquirido algún producto
+de los siguientes rubros: PILAS y PASTILLAS y tengan un limite crediticio menor
+a $ 15000
+*/
+create trigger notificarCliente on Item_factura for insert
+as
+begin
+	declare @cliente char(6)
+	declare c1 cursor for select fact_cliente from inserted 
+						join Factura on item_tipo+item_sucursal+item_numero=fact_tipo+fact_sucursal+fact_numero
+						join Producto on item_producto = prod_codigo
+						join Rubro on prod_rubro = rubr_id
+						where (rubr_detalle = 'PILAS' or rubr_detalle = 'PASTILLAS')
+						group by fact_cliente
+	open c1
+	fetch c1 into @cliente
+	while @@FETCH_STATUS = 0
+	begin 
+		if @cliente in (select clie_codigo from Cliente where clie_limite_credito < 15000)
+			print('Recuerde solicitar su regalo sorpresa en su próxima compra')
+	end
+	close c1
+	deallocate c1
+end
+go
+
+/* 2. Se requiere diseñar e implemetar los objetos necesarios para crear una regla que detecte inconsistencias en
+las ventas en linea. 
+En caso de detectar una incosistencia, debera registrarse el detalle correspondiente en una estructura
+adicional. POr el contrario, si no se encuentra ninguna incosistencia, se debera registrar que la factura ha sido validada
+
+Inconsistencias a considerar:
+    1. Que el valor de fact_total no coincida con la suma de los precios multiplicados por la cantidades que los articulos
+    2. Que se genere una factura con una fecha anterior al día actual
+    3. Que se intente eliminar algun registro de una venta
+*/
+create table verficacionFactura (
+	veri_tipo char(1),
+	veri_sucursal char(4),
+	veri_numero char(8),
+	veri_detalle char(50)
+);
+go
+
+create trigger verificarTotal on Item_factura for insert
+as
+begin
+		
 end
 go
